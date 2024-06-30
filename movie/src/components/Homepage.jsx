8ea89e528d8ebcfe85d069dc17dacad2bf0da72b
Surakshya Bhattarai <suraksha.bhatrai@gmail.com>
2024-06-30 17:19:53 +0545

    sidebar update

--- movie/src/components/Homepage.jsx
+++ movie/src/components/Homepage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import backgroundImage from "../images/bgmain.jpg";
 import Sidebar from './Sidebar';
-import MovieListpage from './movieListpage';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import Slider component if it's imported from somewhere
 
 const HomePage = () => {
   const backgroundStyle = {
@@ -11,14 +14,62 @@ const HomePage = () => {
     backgroundRepeat: 'no-repeat',
   };
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, 
+    slidesToScroll: 1,
+    nextArrow: <div className="text-black">Next</div>,
+    prevArrow: <div className="text-black">Prev</div>,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  }; // Define your settings object here
+
   return (
     <div style={backgroundStyle} className="min-h-screen flex flex-col">
       <div className="flex">
         <Sidebar />
-        <MovieListpage />
+        <main className="fixed top-0 left-60 text-white w-full flex flex-col items-center">
+          <div className="mywidth mb-10 w-full max-w-7xl p-4">
+            <h2 className="text-3xl mb-4 font-bold text-yellow-300">Recommended for you</h2>
+            <Slider {...settings}>
+            <div className="p-2">
+              <div className="w-48 h-50 bg-green-400 overflow-hidden">
+                
+              </div>
+        
+          </div>
+          </Slider>
+          </div>
+          <div className="mb-10 w-full max-w-7xl p-4">
+            <h2 className="text-3xl mb-4 font-bold text-yellow-300">Popular</h2>
+            <Slider {...settings}>
+            <div className="p-2">
+              <div className="w-48 h-50 bg-green-400 overflow-hidden">
+                
+              </div>
+            
+          </div>
+          </Slider>
+          </div>
+        </main>
       </div>
     </div>
   );
 };
 
 export default HomePage;
+
--- movie/src/components/Sidebar.jsx
+++ movie/src/components/Sidebar.jsx
@@ -1,37 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const Sidebar = () => {
   const { adminLoggedIn } = useContext(AuthContext);
-  console.log(adminLoggedIn);
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Close the menu when navigating to another page
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location.pathname]);
 
   return (
     <aside className="h-screen fixed top-0 left-0 w-1/6 bg-gray-900 p-4 flex flex-col justify-between">
       <div className="flex flex-col gap-4">
-        <h1 className="text-red-900 font-bold text-align-center text-4xl">MRS</h1>
-        {adminLoggedIn ? (
-          <nav>
-            <ul>
-              <li className="mb-4 mt-1">
-                <Link to="/home" className="text-white text-lg">
-                  Home
-                </Link>
-              </li>
-              <li className="mb-4">
-                <Link to="/watchlist" className="text-white text-lg">
-                  Watchlist
-                </Link>
-              </li>
-              <li className="mb-4">
-                <Link to="/trending" className="text-white text-lg">
-                  Trending
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        ) : null}
+        <h1 className="text-red-900 font-bold text-align-center text-4xl">
+          MRS
+        
+        {adminLoggedIn && (
+          <button onClick={toggleMenu} className="text-black text-3xl">
+            &#9776;
+          </button>
+        )}
+        </h1>
+        {adminLoggedIn && isOpen && (
+          <div className="bg-gray-800 p-4 rounded-lg">
+            <nav>
+              <ul>
+                <li className="mb-4 mt-1">
+                  <Link to="/home" className="text-white text-lg">
+                    Home
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link to="/watchlist" className="text-white text-lg">
+                    Watchlist
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link to="/trending" className="text-white text-lg">
+                    Trending
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
       </div>
       <div>
         {adminLoggedIn ? (
